1527b2ca94bacae01aedf4628a49e4ac95f8e3ba
Ahmedsaki Abdirahman <skgadhle@gmail.com>
2025-07-27 10:55:29 +0300

    Initial commit

---
+++ assets/js/includes.js
@@ -0,0 +1,9 @@
+fetch('components/header.html').then(res => res.text()).then(data => {
+  document.getElementById("header").innerHTML = data;
+});
+fetch('components/footer.html').then(res => res.text()).then(data => {
+  document.getElementById("footer").innerHTML = data;
+});
+fetch('components/hero.html').then(res => res.text()).then(data => {
+  document.getElementById("hero").innerHTML = data;
+});
